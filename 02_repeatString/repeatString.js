516fb0d2b59c5a1f17a563fa87bdff74e747ce62
serverigit <serverigit@outlook.com>
2024-08-21 21:35:52 +0300

    Create function to repeat a string multiple times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let stringRepeat = string;
+  if (num === 0) {
+    return ``;
+  } else if (num < 0) {
+    return `ERROR`;
+  } else {
+    for (i = 1; i < num; i++) {
+      stringRepeat += string;
+    }
+    return stringRepeat;
+  }
 };
 
 // Do not edit below this line
