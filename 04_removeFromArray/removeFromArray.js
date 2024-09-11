9459fba8686518a94a9084e6d7ba5765bb7341aa
serverigit <serverigit@outlook.com>
2024-09-11 16:36:47 +0300

    add function to remove argument value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arrayInput, args) {
+//     arrayInput.splice(arrayInput.indexOf(args), 1);
+//     return arrayInput;
+// };
+
+const removeFromArray = function(arrayInput, ...args) {
+    args.forEach((element) => {
+        const index = arrayInput.indexOf(element);
+        if (index !== -1) {
+            while (arrayInput.includes(element)) {
+                arrayInput.splice(index, 1);
+            }
+        }
+    });
+    return arrayInput;
 };
 
 // Do not edit below this line
