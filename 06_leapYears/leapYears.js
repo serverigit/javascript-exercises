1fd8ac9b012eca2ae50966950541903eebd1d6b3
serverigit <serverigit@outlook.com>
2024-09-12 16:38:36 +0300

    add alternative function alternative function to return true or false directly instead of assigning true or false to year variable

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,12 +3,23 @@
 // UNLESS it is divisible by 400.
 // must be divisible by 4. 
 
+// Returns year as true or false
+
+// const leapYears = function(year) {
+//     year % 100 === 0 && year % 400 === 0 ? year = true :
+//     year % 100 === 0 ? year = false : 
+//     year % 4 === 0 ? year = true: 
+//     year = false;
+//     return year;
+// };
+
+// Directly returns true or false
+
 const leapYears = function(year) {
-    year % 100 === 0 && year % 400 === 0 ? year = true :
-    year % 100 === 0 ? year = false : 
-    year % 4 === 0 ? year = true: 
-    year = false;
-    return year;
+    if (year % 100 === 0 && year % 400 === 0) return true;
+    if (year % 100 === 0) return false;
+    if (year % 4 === 0) return true;
+    return false;
 };
 
 // Do not edit below this line
