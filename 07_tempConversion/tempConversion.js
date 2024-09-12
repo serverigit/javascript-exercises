60d580b934478c85069b885cc4667846c744e96d
serverigit <serverigit@outlook.com>
2024-09-12 18:17:00 +0300

    add two function to convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// xF = (x-32) * 5/9C
+
+const convertToCelsius = function(fahrenheit) {
+  const celsius = Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+// xC = (x * 9/5 + 32)F
+
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = Math.round((celsius * 9/5 + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
